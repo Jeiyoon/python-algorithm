459e80e0d7994c499814ef4eb9fad8f2b9d8657e
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-05 10:24:48 +0900

    Update 070522_max_formula.py

--- programmers/level2/070522_max_formula.py
+++ programmers/level2/070522_max_formula.py
@@ -9,51 +9,34 @@ ops = {"+": operator.add,
            "*": operator.mul}
 
 def op_calculator(expr_list: List[int], op1: str, op2: str, op3: str) -> int:
-  print("expr_list: ", expr_list)
-
   # op1
-  pop_list = []
-  if op1 in expr_list:
+  while op1 in expr_list:
     for idx in range(len(expr_list)):
       if expr_list[idx] == op1:
-        expr_list[idx-1] = ops[expr_list[idx]](int(expr_list[idx-1]), int(expr_list[idx+1]))
-        expr_list[idx+1] = expr_list[idx-1]
-        pop_list.append(idx)
-        pop_list.append(idx+1)
-    
-  expr_list = [expr for idx, expr in enumerate(expr_list) if not idx in pop_list]
-  print("expr_list: ", expr_list)
+        right = expr_list.pop(idx+1)
+        left = expr_list.pop(idx-1)
+        expr_list[idx-1] = ops[op1](int(left), int(right))
+        break        
 
   # op2
-  pop_list = []
-  if op2 in expr_list:
+  while op2 in expr_list:
     for idx in range(len(expr_list)):
       if expr_list[idx] == op2:
-        expr_list[idx-1] = ops[expr_list[idx]](int(expr_list[idx-1]), int(expr_list[idx+1]))
-        expr_list[idx+1] = expr_list[idx-1]
-        pop_list.append(idx)
-        pop_list.append(idx+1)
-  
-  expr_list = [expr for idx, expr in enumerate(expr_list) if not idx in pop_list]
-  print("expr_list: ", expr_list)
+        right = expr_list.pop(idx+1)
+        left = expr_list.pop(idx-1)
+        expr_list[idx-1] = ops[op2](int(left), int(right))
+        break        
 
   # op3
-  pop_list = []
-  if op3 in expr_list:
+  while op3 in expr_list:
     for idx in range(len(expr_list)):
       if expr_list[idx] == op3:
-        expr_list[idx-1] = ops[expr_list[idx]](int(expr_list[idx-1]), int(expr_list[idx+1]))
-        expr_list[idx+1] = expr_list[idx-1]
-        pop_list.append(idx)
-        pop_list.append(idx+1)
-  
-  # exception: last operator
-  # print("pop_list: ",pop_list)
-  expr_list = [expr for expr in expr_list if str(type(expr)) == "<class 'int'>" ]
-  print("expr_list: ", expr_list)
-  print(" ")
+        right = expr_list.pop(idx+1)
+        left = expr_list.pop(idx-1)
+        expr_list[idx-1] = ops[op3](int(left), int(right))
+        break        
 
-  return max(map(abs,expr_list))
+  return abs(expr_list[-1])
 
 def solution(expression: str) -> int:
     answer_list = []
@@ -71,15 +54,15 @@ def solution(expression: str) -> int:
     # 2. calculate
     # 2 - (1): "+" > "-" > "*"
     answer_list.append(op_calculator(deepcopy(expr_list), "+", "-", "*"))
-    # 2 - (2): "+" > "*" > "-"
+    # # 2 - (2): "+" > "*" > "-"
     answer_list.append(op_calculator(deepcopy(expr_list), "+", "*", "-"))
-    # 2 - (3): "-" > "+" > "*"
+    # # 2 - (3): "-" > "+" > "*"
     answer_list.append(op_calculator(deepcopy(expr_list), "-", "+", "*"))
-    # 2 - (4): "-" > "*" > "+"
+    # # 2 - (4): "-" > "*" > "+"
     answer_list.append(op_calculator(deepcopy(expr_list), "-", "*", "+"))
-    # 2 - (5): "*" > "+" > "-"
+    # # 2 - (5): "*" > "+" > "-"
     answer_list.append(op_calculator(deepcopy(expr_list), "*", "+", "-"))
-    # 2 - (6): "*" > "-" > "+"
+    # # 2 - (6): "*" > "-" > "+"
     answer_list.append(op_calculator(deepcopy(expr_list), "*", "-", "+"))
 
     return max(answer_list)
