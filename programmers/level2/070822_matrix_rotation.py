e5506a2ad9b291e7433cce431aa315756a7c60dc
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-07 11:37:07 +0900

    Update and rename 070722_matrix_rotation.py to 070822_matrix_rotation.py

--- programmers/level2/070822_matrix_rotation.py
+++ programmers/level2/070822_matrix_rotation.py
@@ -1,4 +1,4 @@
-# date: 2022.07.07
+# date: 2022.07.08
 # author: jeiyoon
 # https://velog.io/@sjy5386/Python-2%EC%B0%A8%EC%9B%90-%EB%B0%B0%EC%97%B4-%EC%84%A0%EC%96%B8%ED%95%98%EA%B8%B0
 from typing import List
@@ -24,13 +24,13 @@ def solution(rows: int, columns: int, queries: List[List[int]]) -> List[int]:
     while not done:  
       # phase 1: →
       if phase == 1:
-        for idx in range(mini_col - 1): # 0 1
+        for idx in range(mini_col - 1): 
           matrix[x1][y2 - idx] = matrix[x1][y2 - idx - 1]
         phase = 2
 
       # phase 2: ↑
       if phase == 2:
-        for idx in range(mini_row - 1): # 0 1 2
+        for idx in range(mini_row - 1):
           matrix[x1 + idx][y1] = matrix[x1 + idx + 1][y1]
         phase = 3
 
@@ -50,29 +50,34 @@ def solution(rows: int, columns: int, queries: List[List[int]]) -> List[int]:
       done = True
     
     # 3. min number
-    for y in range(y1, y2 + 1):
-      for x in range(x1, x2 + 1):
+    for x in range(x1, x2 + 1):
+      for y in range(y1, y2 + 1):
         # exception: central hole
-        answer_candidate.append(matrix[x][y])
-    print("answer_candidate: ", answer_candidate)
-    print("min: ", min(answer_candidate))
+        if y == y1 or y == y2: 
+          answer_candidate.append(matrix[x][y])
+        elif x == x1 or x == x2:
+          answer_candidate.append(matrix[x][y])
+        else:
+          pass
+    # print("answer_candidate: ", answer_candidate)
+    # print("min: ", min(answer_candidate))
     answer.append(min(answer_candidate))
 
   return answer
 
-# # result = [8, 10, 25]
-# rows = 6
-# columns = 6
-# queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
+# result = [8, 10, 25]
+rows = 6
+columns = 6
+queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
 
 # # result = [1, 1, 5, 3]
 # rows = 3
 # columns = 3
 # queries = [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]
 
-# result = [1]
-rows = 100
-columns = 97
-queries = [[1,1,100,97]]
+# # result = [1]
+# rows = 100
+# columns = 97
+# queries = [[1,1,100,97]]
 
 print(solution(rows, columns, queries))
