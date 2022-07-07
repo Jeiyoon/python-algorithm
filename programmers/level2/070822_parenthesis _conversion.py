f690b24d741b1d2fc7c624f267d566dde2f4d31a
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-08 08:58:09 +0900

    Update and rename 1014_parenthesis _conversion.py to 070822_parenthesis _conversion.py

---
+++ programmers/level2/070822_parenthesis _conversion.py
@@ -0,0 +1,15 @@
+# date: 2022.07.08
+# author: jeiyoon
+def solution(p):
+  return p
+
+# result = "(()())()"
+p = "(()())()"
+
+# result = 	"()"
+# p = ")("
+
+# result = "()(())()"
+# p = "()))((()"
+
+print(solution(p))
