4450a15b143f26d47be52f26002dc9c98505eea6
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-07 12:56:30 +0900

    Update and rename 070822_matrix_rotation.py to 070722_matrix_rotation.py

--- programmers/level2/070722_matrix_rotation.py
+++ programmers/level2/070722_matrix_rotation.py
@@ -1,4 +1,4 @@
-# date: 2022.07.08
+# date: 2022.07.07
 # author: jeiyoon
 # https://velog.io/@sjy5386/Python-2%EC%B0%A8%EC%9B%90-%EB%B0%B0%EC%97%B4-%EC%84%A0%EC%96%B8%ED%95%98%EA%B8%B0
 from typing import List
@@ -8,8 +8,8 @@ def solution(rows: int, columns: int, queries: List[List[int]]) -> List[int]:
   answer = []
 
   # 1. (rows x cols) matrix
-  matrix = [[j+i+1 for j in range(columns)] for i in range(0, rows * rows, rows)]
-
+  matrix = [[j + (columns * i) for j in range(1, columns + 1)] for i in range(0, rows)]
+  
   # 2. rotation
   for query in queries:
     answer_candidate = []
@@ -53,27 +53,26 @@ def solution(rows: int, columns: int, queries: List[List[int]]) -> List[int]:
     for x in range(x1, x2 + 1):
       for y in range(y1, y2 + 1):
         # exception: central hole
-        if y == y1 or y == y2: 
+        if x == x1 or x == x2:
           answer_candidate.append(matrix[x][y])
-        elif x == x1 or x == x2:
-          answer_candidate.append(matrix[x][y])
-        else:
-          pass
-    # print("answer_candidate: ", answer_candidate)
-    # print("min: ", min(answer_candidate))
+        else: 
+          if y == y1 or y == y2: 
+            answer_candidate.append(matrix[x][y])
+          else:
+            pass
     answer.append(min(answer_candidate))
 
   return answer
 
-# result = [8, 10, 25]
-rows = 6
-columns = 6
-queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
+# # result = [8, 10, 25]
+# rows = 6
+# columns = 6
+# queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
 
-# # result = [1, 1, 5, 3]
-# rows = 3
-# columns = 3
-# queries = [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]
+# result = [1, 1, 5, 3]
+rows = 3
+columns = 5
+queries = [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]
 
 # # result = [1]
 # rows = 100
