7c11f069e40f9e6878662c607445854b78293526
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-12 23:28:36 +0900

    Update 071222_elimination_by_grouping.py

--- programmers/level2/071222_elimination_by_grouping.py
+++ programmers/level2/071222_elimination_by_grouping.py
@@ -1,30 +1,32 @@
 # date: 2022.07.12
 # author: jeiyoon
-# 타임아웃
-from copy import deepcopy
 
 def solution(s: str) -> int:
+  # exception: odd num
+  if len(s) % 2 == 1:
+    return 0
+
+  # Length of string : natural number less than 1,000,000.
   s = list(s)
-  
-  while len(s) != 0: 
-    # print("s: ", s)
-    before_s = deepcopy(s)
+  stack = []
 
-    for idx in range(len(s) - 1):
-      if s[idx] == s[idx + 1]:
-        s.pop(idx)
-        s.pop(idx)
-        break
-    
-    if before_s == s:
-      return 0
+  for char in s:
+    if len(stack) >= 1 and stack[-1] == char:
+      stack.pop()
+    else:
+      stack.append(char)        
 
-  return 1
+  if len(stack) == 0:
+    return 1
+  else:
+    return 0
 
 # result = 1
-# s = 'baabaa'
+s = 'baabaa'
+# s = 'aaaabaabaaaaaa'
 
 # result = 0
-s = 'cdcd'
+# s = 'cdcdcdcdcdcdcdcdcdcdcdcd'
+# s = "a"
 
 print(solution(s))
