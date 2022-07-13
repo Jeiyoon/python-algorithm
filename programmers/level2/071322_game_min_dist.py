a5c24ffabd2108eea0135c4325ce4020e7c18675
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-13 12:59:11 +0900

    Update 071322_game_min_dist.py

--- programmers/level2/071322_game_min_dist.py
+++ programmers/level2/071322_game_min_dist.py
@@ -15,17 +15,89 @@ from typing import List
 
 def solution(maps: List[List[int]]) -> int:
   answer = 0
+
+  x_coordinate = len(maps) - 1 # 5
+  y_coordinate = len(maps[0]) - 1 # 5
+  print(x_coordinate)
+
+  while x_coordinate != 0 and y_coordinate != 0:
+    print("####################################")
+    print("x_coordinate: ", x_coordinate)
+    print("y_coordinate: ", y_coordinate)
+    # 1. check current position
+    dir_list = []
+    # left
+    if y_coordinate > 0:
+      dir_list.append('left')
+    # up
+    if x_coordinate > 0:
+      dir_list.append('up')
+    # right
+    if y_coordinate < 4:
+      dir_list.append('right')
+    # down
+    if x_coordinate < 4:
+      dir_list.append('down')
+
+    print("dir_list: ", dir_list)
+
+    # 2. check whether wall or road
+    # left
+    if 'left' in dir_list:
+      if maps[x_coordinate][y_coordinate - 1] == 0:
+        dir_list.remove('left')
+    # up
+    if 'up' in dir_list:
+      if maps[x_coordinate - 1][y_coordinate] == 0:
+        dir_list.remove('up')
+    # right
+    if 'right' in dir_list:
+      if maps[x_coordinate][y_coordinate + 1] == 0:
+        dir_list.remove('right')
+    # down
+    if 'down' in dir_list:
+      if maps[x_coordinate + 1][y_coordinate] == 0:
+        dir_list.remove('down')
+
+    print("dir_list: ", dir_list)
+    
+    # 3. if there's no road, return -1
+    if len(dir_list) == 0:
+      return -1
+
+    # 4. go with "left" and "up" priority 
+    if 'left' in dir_list:
+      y_coordinate -= 1
+      answer += 1
+      continue
+    if 'up' in dir_list:
+      x_coordinate -= 1
+      answer += 1
+      continue
+    if 'down' in dir_list:
+      y_coordinate += 1
+      answer += 1
+      continue
+    if 'right' in dir_list:
+      x_coordinate += 1
+      answer += 1
+      continue
+    
   return answer
 
-# result = 	11
+# result = 11
 maps = [[1,0,1,1,1],
         [1,0,1,0,1],
         [1,0,1,1,1],
         [1,1,1,0,1],
         [0,0,0,0,1]]
 
-# result = 	-1
-# maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+# result = -1
+# maps = [[1,0,1,1,1],
+#         [1,0,1,0,1],
+#         [1,0,1,1,1],
+#         [1,1,1,0,0],
+#         [0,0,0,0,1]]
 
 
 print(solution(maps))
