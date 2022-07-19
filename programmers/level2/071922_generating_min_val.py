58dbecf7dc0f401e545d42dbd27f865d6c0f4645
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-07-19 17:20:27 +0900

    Update 071922_generating_min_val.py

--- programmers/level2/071922_generating_min_val.py
+++ programmers/level2/071922_generating_min_val.py
@@ -1,1 +1,27 @@
-pass
+# date: 2022.07.19
+# author: jeiyoon
+from typing import List
+
+def solution(A: List[int],B: List[int]) -> int:
+  # Length of array "A" and "B" : natural number less than 1,000.
+  # Each element of array "A" and "B" : natural number less than 1,000.
+  answer = 0
+
+  # "A" and "B" that have the same length. 
+  A = sorted(A)
+  B = sorted(B, reverse=True)
+
+  for a, b in zip(A, B):
+    answer += a*b
+
+  return answer
+
+# result = 29
+A = [1, 4, 2]  # 1 - 4 - 2
+B = [5, 4, 4]  # 5 - 4 - 4
+
+# result = 10
+# A = [1,2]  # 1 - 2 
+# B = [3,4]  # 4 - 3
+
+print(solution(A, B))
